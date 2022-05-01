cffd8380545a76cab97b278d2cc5e4b2890981e7
NikitaVirachev <guardsolder@gmail.com>
2022-05-01 20:53:56 +0300

    feat: complete task mine sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function minesweeper( matrix ) {
+  let sapperField = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    sapperField.push([]);
+    for (let j = 0; j < matrix[i].length; j++) {
+      let countMine = 0;
+      
+      if (i + 1 < matrix.length) {
+        if (matrix[i + 1][j]) countMine++;
+        if (j + 1 < matrix.length) {
+          if (matrix[i][j + 1]) countMine++;
+          if (matrix[i + 1][j + 1]) countMine++;
+        }
+        if (j - 1 >= 0) {
+          if (matrix[i][j - 1]) countMine++;
+          if (matrix[i + 1][j - 1]) countMine++;
+        }
+      }
+      if (i - 1 >= 0) {
+        if (matrix[i - 1][j]) countMine++;
+        if (j + 1 < matrix.length) {
+          if (matrix[i - 1][j + 1]) countMine++;
+        }
+        if (j - 1 >= 0) {
+          if (matrix[i - 1][j - 1]) countMine++;
+        }
+      }
+
+      sapperField[i].push(countMine)
+    }
+  }
+
+  return sapperField;
 }
 
 module.exports = {
